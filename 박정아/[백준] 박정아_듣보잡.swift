dff9a12a9ada1496e32a0cc8ef99da338b26c2d2
co3oing <co3oing@naver.com>
2021-12-16 14:55:23 +0900

    modify [프로그래머스] 박정아_듣보잡.swift

--- 박정아/[백준] 박정아_듣보잡.swift
+++ 박정아/[백준] 박정아_듣보잡.swift
@@ -1,8 +1,28 @@
-//
-//  [백준] 박정아_듣보잡.swift
-//  BOJ
-//
-//  Created by 박정아 on 2021/11/21.
-//
+func solution() {
+    let input = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    let n = input[0]
+    let m = input[1]
+    var nArr = Set<String>()
+    var resultArr = [String]()
+    
+    for _ in 0..<n {
+        nArr.insert(readLine()!)
+    }
+    
+    for _ in 0..<m {
+        let name = readLine()!
+        if nArr.contains(name) {
+            resultArr.append(name)
+        }
+    }
+    
+    resultArr.sort()
+    print(resultArr.count)
+    
+    resultArr.forEach { name in
+        print(name)
+    }
+}
 
-import Foundation
+solution()
+
