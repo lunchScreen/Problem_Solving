90ecd5e6c784187cad9ff8cce916cc102e265ba5
JISU LEE <64150179+tmfrlrkvlek@users.noreply.github.com>
2022-01-22 21:20:16 +0900

    solve Baekjoon_17626.py

---
+++ 이지수/Python/Baekjoon_17626.py
@@ -0,0 +1,11 @@
+import sys
+def check(n) :
+    if (n**0.5).is_integer() : return 1
+    for i in range(1, int(n**0.5)+1) :
+        if (int(n-i*i)**0.5).is_integer() : return 2
+    for i in range(1, int(n**0.5)+1) :
+        for j in range(1, int((n-i*i)**0.5)+1) :
+            if ((n-i*i-j*j)**0.5).is_integer() : return 3
+    return 4
+n = int(sys.stdin.readline())
+print(check(n))
