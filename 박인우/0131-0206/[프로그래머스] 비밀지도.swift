f63480c19ece452020e8902baf786262aa8f0841
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-05 20:47:09 +0900

    Update [프로그래머스] 비밀지도.swift

--- 박인우/0131-0206/[프로그래머스] 비밀지도.swift
+++ 박인우/0131-0206/[프로그래머스] 비밀지도.swift
@@ -1,3 +1,17 @@
+// 방법 1
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    for idx in 0..<n {
+        var str = String(arr1[idx] | arr2[idx], radix: 2)
+        str = String(repeating: "0", count: n-str.count) + str
+        str = str.replacingOccurrences(of: "0", with: " ")
+        str = str.replacingOccurrences(of: "1", with: "#")
+        answer.append(str)
+    }
+    return answer
+}
+
+// 방법 2
 func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     var answer: [String] = []
     for idx in 0..<n {
@@ -8,3 +22,17 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
     }
     return answer
 }
+
+// 방법 3
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    var answer: [String] = []
+    for idx in 0..<n {
+        var str = String(arr1[idx] | arr2[idx], radix: 2)
+        str = String(repeating: "0", count: n-str.count) + str
+        str = String(str.map { $0 == "1" ? "#" : " " })
+        answer.append(str)
+    }
+    return answer
+}
+
+// 방법 4
