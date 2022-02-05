4a762c3bcb4102ca04882658614acd4723556e8c
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-05 20:47:19 +0900

    Update [프로그래머스] 비밀지도.swift

--- 박인우/0131-0206/[프로그래머스] 비밀지도.swift
+++ 박인우/0131-0206/[프로그래머스] 비밀지도.swift
@@ -36,3 +36,10 @@ func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
 }
 
 // 방법 4
+func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+    return (0..<n).map {
+        String(String(arr1[$0] | arr2[$0] | 2 << (n - 1), radix: 2).map {
+            $0 == "1" ? "#" : " "
+        }[1...n])
+    }
+}
