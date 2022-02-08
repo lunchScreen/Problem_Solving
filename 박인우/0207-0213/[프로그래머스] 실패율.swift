cefd032ea845f04ad0471d8260edfd31a31c4132
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-08 15:42:58 +0900

    Update [프로그래머스] 실패율.swift

--- 박인우/0207-0213/[프로그래머스] 실패율.swift
+++ 박인우/0207-0213/[프로그래머스] 실패율.swift
@@ -1,3 +1,27 @@
+import Foundation
+
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var stop: [Double] = Array(repeating: 0, count: N+1)
+    
+    for stage in stages {
+        stop[stage-1] += 1
+    }
+    
+    var arrival = (0...N-1).map { 
+        stop[$0...N].reduce(Double(0),+)
+    }
+    
+    var failure = zip(stop,arrival).map {
+        $1 > 0 ? $0 / $1 : $0
+    }.enumerated().sorted {
+        $0.element > $1.element
+    }.map {
+        $0.offset + 1
+    }
+    
+    return failure
+}
+
 func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     var rate = [Int:Float]()
     let dic = stages.reduce(into: [Int:Int]()) { $0[$1, default: 0] += 1 }
