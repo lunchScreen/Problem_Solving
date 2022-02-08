a13379b796e084d53841cce65934d61bccc32ee5
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-08 15:42:40 +0900

    Create [프로그래머스] 실패율.swift

---
+++ 박인우/0207-0213/[프로그래머스] 실패율.swift
@@ -0,0 +1,10 @@
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+    var rate = [Int:Float]()
+    let dic = stages.reduce(into: [Int:Int]()) { $0[$1, default: 0] += 1 }
+
+    for n in 1...N {
+        rate[n] = Float(dic[n] ?? 0) / Float(dic.filter { n < $0.key }.map { $0.value }.reduce(0, +))
+    }
+
+    return rate.sorted(by: <).sorted { $0.value > $1.value }.map { $0.key }
+}
