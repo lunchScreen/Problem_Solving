43ba61bed1cff706b6d2e36263d0afbcbd95ed83
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-17 15:58:28 +0900

    Update [프로그래머스] 가장 큰 수.swift

--- 박인우/0214-0220/[프로그래머스] 가장 큰 수.swift
+++ 박인우/0214-0220/[프로그래머스] 가장 큰 수.swift
@@ -1,7 +1,19 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    return numbers.sorted(by: {
+    var result = numbers.sorted(by: {
         return Int("\($0)\($1)")! > Int("\($1)\($0)")!
     }).map { String($0) }.joined()
+    
+    return result.first! == "0" ? "0" : result
+}
+
+func solution(_ numbers:[Int]) -> String {
+    let result = numbers.map { String($0) }.sorted(by: {
+        let one = $0+$0+$0
+        let two = $1+$1+$1
+        return one > two
+    }).joined()
+    
+    return result.first! == "0" ? "0" : result
 }
