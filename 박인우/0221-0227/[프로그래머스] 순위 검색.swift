14f756b3f74bfae416df8c298ab88b4b89b31b8a
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-02-24 22:38:04 +0900

    Update [프로그래머스] 순위 검색.swift

--- 박인우/0221-0227/[프로그래머스] 순위 검색.swift
+++ 박인우/0221-0227/[프로그래머스] 순위 검색.swift
@@ -90,3 +90,64 @@ func solution(_ info:[String], _ query:[String]) -> [Int] {
     
     return result
 }
+
+// 효율성 모두 통과
+
+import Foundation
+
+func solution(_ info:[String], _ query:[String]) -> [Int] {
+    var dic = [String:[Int]]()
+    
+    info.forEach {    
+        var dfs = [(0,$0.components(separatedBy: " "))]
+        
+        while dfs.count > 0 {
+            var now = dfs.removeLast()
+            let idx = now.0
+            var lst = now.1
+            
+            if idx == 4 {
+                let key = lst[0...3].joined()
+                dic[key, default: [Int]()].append(Int(lst[4])!)
+                continue
+            }
+            
+            dfs.append((idx+1,lst))
+            lst[idx] = "-"
+            dfs.append((idx+1,lst))
+        }
+    }
+    
+    for key in dic.keys {
+        dic[key] = dic[key]!.sorted(by: >)
+    }
+    
+    let result: [Int] = query.map { str in
+        let lst = str.components(separatedBy: " ")
+        let key = lst[0] + lst[2] + lst[4] + lst[6]
+        let limitScore = Int(lst[7])!
+        if let candidates = dic[key] {
+            var start = 0
+            var end = candidates.count
+            var idx = (start + end) / 2
+            
+            while start < end {
+                idx = (start + end) / 2
+                if candidates[idx] >= limitScore {
+                    if idx + 1 >= candidates.count || (candidates[idx+1] < limitScore) {
+                        return idx+1
+                    }
+                    start = idx + 1
+                } else {
+                    end = idx
+                }
+            }
+            
+            return idx
+        } else {
+            return 0
+        }
+    }
+    
+    return result
+}
