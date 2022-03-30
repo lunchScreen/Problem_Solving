2d56c158fb083f7e53fea2ed0da5aa2fd923ebee
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-03-30 17:25:34 +0900

    Create [프로그래머스] 다음 큰 숫자.swift

---
+++ 박인우/0328-0403/[프로그래머스] 다음 큰 숫자.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var now = n
+    let binary = String(n, radix: 2)
+    let oneCount = binary.reduce(0, {
+        if $1 == "1" {
+            return $0 + 1
+        } else {
+            return $0
+        }
+    })
+    
+    while true {
+        now += 1
+        let nowBinary = String(now, radix: 2)
+        let nowOneCount = nowBinary.reduce(0, {
+            if $1 == "1" {
+                return $0 + 1
+            } else {
+                return $0
+            }
+        })
+        
+        if nowOneCount == oneCount {
+            return now
+        }
+    }
+    
+    return -1
+}
