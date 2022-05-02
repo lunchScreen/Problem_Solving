737cfe7477fda0a16b8912572f0c8fc63903a6e5
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-05-02 15:53:26 +0900

    Update [프로그래머스] 추석 트래픽.swift

--- 박인우/0502-0508/[프로그래머스] 추석 트래픽.swift
+++ 박인우/0502-0508/[프로그래머스] 추석 트래픽.swift
@@ -22,8 +22,7 @@ func solution(_ lines:[String]) -> Int {
 }
 
 func solution(_ lines:[String]) -> Int {
-    var queue = [(start: Double,end: Double)]()
-    var maxCount = 0
+    var times = [(start: Double,end: Double)]()
     
     for line in lines {
         var data = line.components(separatedBy: " ")
@@ -32,11 +31,21 @@ func solution(_ lines:[String]) -> Int {
         let endTime = data[1].components(separatedBy: ":")
         let totalEndSecond = (Double(endTime[0])!*3600) + (Double(endTime[1])!*60) + Double(endTime[2])!
         let totalStartSecond = totalEndSecond - Double(data[2])! + 0.001
-        while queue.count > 0 && queue[0].end+0.999 < totalStartSecond {
-            queue.removeFirst()
-        } 
-        queue.append((totalStartSecond,totalEndSecond))
-        maxCount = max(queue.count, maxCount)
+        
+        times.append((totalStartSecond,totalEndSecond))
+    }
+    
+    var maxCount = 0
+    for time in times {
+        var count = [(start: Double,end: Double)]()
+        for time_ in times {
+            if (time.end <= time_.start && time.end + 1 > time_.start) ||
+                (time.end <= time_.end && time.end + 1 > time_.end) ||
+                (time.end >= time_.start && time.end + 1 < time_.end) {
+                count.append(time_)
+            }
+        }
+        maxCount = max(count.count, maxCount)
     }
     
     return maxCount
