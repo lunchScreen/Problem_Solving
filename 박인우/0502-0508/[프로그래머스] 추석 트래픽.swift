75ee3e0476558178d0a8819923bffd5916901123
inuinseoul <70463738+inuinseoul@users.noreply.github.com>
2022-05-02 15:17:51 +0900

    Create [프로그래머스] 추석 트래픽.swift

---
+++ 박인우/0502-0508/[프로그래머스] 추석 트래픽.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ lines:[String]) -> Int {
+    var queue = [(start: Double,end: Double)]()
+    var maxCount = 0
+    
+    for line in lines {
+        var data = line.components(separatedBy: " ")
+        data[2].removeLast()
+        
+        let endTime = data[1].components(separatedBy: ":")
+        let totalEndSecond = (Double(endTime[0])!*3600) + (Double(endTime[1])!*60) + Double(endTime[2])!
+        let totalStartSecond = totalEndSecond - Double(data[2])! + 0.001
+        while queue.count > 0 && queue[0].end+0.999 < totalStartSecond {
+            queue.removeFirst()
+        } 
+        queue.append((totalStartSecond,totalEndSecond))
+        maxCount = max(queue.count, maxCount)
+    }
+    
+    return maxCount
+}
